4139e7991162cdb678bada0d9e0b64bbf34ac206
Gustavo Jose Andriani <gustavo.joseandriani@gmail.com>
2023-10-22 14:21:03 -0300

    Requisicao a API

--- src/pages/Filmes.jsx
+++ src/pages/Filmes.jsx
@@ -1,36 +1,32 @@
-import { useContext } from "react"
-import PostContext from "../contexts/PostContext"
-import Posts from "../components/Posts"
+// import Posts from "../components/Posts"
 
 export default function Filmes() {
-    const postList = useContext(PostContext)
-
     return (
         <>
-            <h2>Séries</h2>
+            <h2>Filmes</h2>
 
             <div style={{ borderTop: "1px solid #ED7D31", margin: "2rem 0" }}></div>
 
-            <div className="AppContainer">       
-                {
-                    postList.length > 0 ? (postList.map((post) => {
-                        if(post.typePost === "Filme") {
-                            return (
-                                <Posts
-                                    key={post.key}
-                                    thumbPost={post.thumbPost}
-                                    titlePost={post.titlePost}
-                                    typePost={post.typePost}
-                                />
-                            )
-                        }
-                        else {
-                            return null
-                        }
-                    })
-                    ) : null
-                }
-            </div>   
+            {/* <div className="AppContainer">        */}
+                {/* { */}
+                    {/* postList.length > 0 ? (postList.map((post) => { */}
+                        {/* if(post.typePost === "Filme") { */}
+                            {/* return ( */}
+                                {/* <Posts */}
+                                    {/* key={post.key} */}
+                                    {/* thumbPost={post.thumbPost} */}
+                                    {/* titlePost={post.titlePost} */}
+                                    {/* typePost={post.typePost} */}
+                                {/* /> */}
+                            {/* ) */}
+                        {/* } */}
+                        {/* else { */}
+                            {/* return null */}
+                        {/* } */}
+                    {/* }) */}
+                    {/* ) : null */}
+                {/* } */}
+            {/* </div>    */}
         </>
     )
 }
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,8 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation"
 import UserContext from "../contexts/UserContext"
-import PostList from "../database/PostList";
-import PostContext from "../contexts/PostContext";
 import UserList from "../database/UserList";
 
 export default function RootLayout() {
@@ -10,14 +8,12 @@ export default function RootLayout() {
     return (
         <>
             <UserContext.Provider value={UserList}>
-                <PostContext.Provider value={PostList}>
-                    <Navigation />
-                    <div style={{
-                        marginLeft: "170px"
-                    }}>
-                        <Outlet />
-                    </div>
-                </PostContext.Provider>
+                <Navigation />
+                <div style={{
+                    marginLeft: "170px"
+                }}>
+                    <Outlet />
+                </div>
             </UserContext.Provider>
         </>
     )
